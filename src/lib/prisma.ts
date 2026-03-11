a6b6104ea0d1d11bc1e617b05b577040ce1dab62
25LP5308 25LP5308 <telmuunotgonbileg@gmail.com>
2026-03-11 18:42:41 +0800

    nerde

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,7 @@
-import { PrismaClient } from "@/generated/prisma/client";
+;
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client";
 const adapter = new PrismaPg({
   connectionString: process.env.PRISMA_URL_DATA_BASE,
 });
